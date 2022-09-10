3727e8bff085591dd2f72a9dce8bf1e64aed2e2a
Stephen Muema <muema1476@gmail.com>
2022-09-10 14:54:21 +0300

    database

--- app/src/main/java/com/example/databaserecycle/MainActivity.kt
+++ app/src/main/java/com/example/databaserecycle/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.databaserecycle
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import java.util.jar.Attributes
+
 
 class MainActivity : AppCompatActivity() {
+  EditText name,email,age;
+  Button insert, view;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name =findViewById(R.id.name);
+        email =findViewById(R.id.email);
+        age=findViewById(R.id.age);
+        insert=findViewById<(R.id.btninsert)
+        view=findViewById(R.id.btnview)
+
+        view.setOnClickListListener(view. )
+
+
+
     }
 }
