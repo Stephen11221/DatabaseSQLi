12bb218a7f854e69ca118beb1ba2d933e23182e7
Stephen Muema <muema1476@gmail.com>
2022-09-15 11:49:55 +0300

    database

--- app/src/main/java/com/example/databaserecycle/MainActivity.kt
+++ app/src/main/java/com/example/databaserecycle/MainActivity.kt
@@ -3,23 +3,35 @@ package com.example.databaserecycle
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintSet
+import com.example.databaserecycle.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.sql.DatabaseMetaData
 import java.util.jar.Attributes
 
 
 class MainActivity : AppCompatActivity() {
-  EditText name,email,age;
-  Button insert, view;
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+      private lateinit var binding: ActivityMainBinding
+      private lateinit var database: DatabaseReference
+
+      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        name =findViewById(R.id.name);
-        email =findViewById(R.id.email);
-        age=findViewById(R.id.age);
-        insert=findViewById<(R.id.btninsert)
-        view=findViewById(R.id.btnview)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        view.setOnClickListListener(view. )
+          binding.loginbtn.setOnClickListener{
+
+              var name =binding.name.text.toString()
+              var email =binding.email.text.toString()
+              var age=binding.age.text.toString()
+
+          }
+
+
 
 
 
---
+++ app/src/main/java/com/example/databaserecycle/User.kt
@@ -0,0 +1,9 @@
+package com.example.databaserecycle
+
+import android.provider.ContactsContract
+
+data class User(val name : String?=null, val email: String?=null, val age: String?=null){
+
+
+
+}
